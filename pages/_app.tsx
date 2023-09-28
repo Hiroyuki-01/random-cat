1f5fc752749cc881a8cdc616ebfcf59ddacde8a2
Hiroyuki-01 <hiroyuki0228n@gmail.com>
2023-09-28 14:27:31 +0900

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+// pages/_app.tsx
+import { GA_TRACKING_ID, pageview } from 'lib/gtag';
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
+  // useEffectでurlの動きを検知
+  useEffect(() => {
+    // GA_TRACKING_ID が設定されていない場合は、return
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+}
